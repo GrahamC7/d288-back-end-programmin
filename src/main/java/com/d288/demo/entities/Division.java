a57541a3d61561f91bdcf02ef1b0706881bb21f4
Graham Cockerham <scocker@wgu.edu>
2024-11-20 15:18:50 -0800

    Division.java, lines 32-43, adding fix for front end not populating

--- src/main/java/com/d288/demo/entities/Division.java
+++ src/main/java/com/d288/demo/entities/Division.java
@@ -29,9 +29,18 @@ public class Division {
     @Column(name = "last_update")
     private Date last_update;
 
-    @Column(name = "country_id")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false, insertable = false, updatable = false)
     private Country country;
+
+    @Column(name = "country_id")
+    private long country_id;
+
+    // TODO fix for front end not populating division
+    public void setCountry(Country country) {
+        setCountry_id(country.getId());
+        this.country = country;
+    }
 
     public Division(){
 
