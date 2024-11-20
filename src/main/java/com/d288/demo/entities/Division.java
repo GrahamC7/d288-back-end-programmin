f7aa4965eec1b0a6dbd60bb8c9857af5da462510
Graham Cockerham <scocker@wgu.edu>
2024-11-20 12:32:19 -0800

    created entity file Country.java, added lines 1-24 to Division.java

--- src/main/java/com/d288/demo/entities/Division.java
+++ src/main/java/com/d288/demo/entities/Division.java
@@ -1,4 +1,24 @@
 package com.d288.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "division")
+@Data
+
 public class Division {
+
+    private Long id;
+
+    private String division_name;
+
+    private Date create_date;
+
+    private Date last_update;
+
+    private Country country;
 }
