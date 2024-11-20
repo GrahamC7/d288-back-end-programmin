4842b8c286f9634f83755195ecc34beb0384688b
Graham Cockerham <scocker@wgu.edu>
2024-11-20 12:34:38 -0800

    Country.java, added lines 1-26

--- src/main/java/com/d288/demo/entities/Country.java
+++ src/main/java/com/d288/demo/entities/Country.java
@@ -1,4 +1,26 @@
 package com.d288.demo.entities;
 
+package com.d288.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "country")
+@Data
+
 public class Country {
+
+    private Long id;
+
+    private String country_name;
+
+    private Date create_date;
+
+    private Date last_update;
+
+    private Set<Division> divisions;
 }
